d74a54be19b19b832a548ca6884939d76d979cbd
Jenna Worthen <worthenj.281@gmail.com>
2025-04-10 17:27:59 -0600

    Update Inventory.js

--- frontendsrc/components/Inventory.js
+++ frontendsrc/components/Inventory.js
@@ -1,1 +1,20 @@
+import React from 'react';
 
+function Inventory({ inventory }) {
+  return (
+    <div className="Inventory">
+      <h2>Your Inventory</h2>
+      {inventory.length === 0 ? (
+        <p>Your inventory is empty.</p>
+      ) : (
+        <ul>
+          {inventory.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Inventory;
