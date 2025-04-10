58f5f04d669e0a0cca408d8bbce48601c4496eea
Jenna Worthen <worthenj.281@gmail.com>
2025-04-10 17:27:35 -0600

    Update GameWindow.js

--- frontendsrc/components/GameWindow.js
+++ frontendsrc/components/GameWindow.js
@@ -1,1 +1,22 @@
+import React from 'react';
 
+function GameWindow({ roomDescription, items }) {
+  return (
+    <div className="GameWindow">
+      <h2>Room Description</h2>
+      <p>{roomDescription}</p>
+      {items.length > 0 && (
+        <>
+          <h3>Items in the room:</h3>
+          <ul>
+            {items.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default GameWindow;
